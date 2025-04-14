0fa56cc267e66f3001a27801c0932caf38af2ea1
Ehiz-js <azamegbesamuel@gmail.com>
2025-04-14 02:58:15 +0100

    added keyboard event and click animations

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,39 +2,53 @@ buttons = document.querySelectorAll(".drum");
 numberOfButtons = buttons.length;
 for (i = 0; i < numberOfButtons; i++) {
 	buttons[i].addEventListener("click", function () {
-		buttonText = this.innerHTML;
-		switch (buttonText) {
-			case "w":
-				let tom1 = new Audio("./sounds/tom-1.mp3");
-				tom1.play();
-				break;
-			case "a":
-				let tom2 = new Audio("./sounds/tom-2.mp3");
-				tom2.play();
-				break;
-			case "s":
-				let tom3 = new Audio("./sounds/tom-3.mp3");
-				tom3.play();
-				break;
-			case "d":
-				let tom4 = new Audio("./sounds/tom-4.mp3");
-				tom4.play();
-				break;
-			case "j":
-				let crash = new Audio("./sounds/crash.mp3");
-				crash.play();
-				break;
-			case "k":
-				let kick = new Audio("./sounds/kick-bass.mp3");
-				kick.play();
-				break;
-			case "l":
-				let snare = new Audio("./sounds/snare.mp3");
-				snare.play();
-				break;
-			default:
-				console.log(innerHTML);
-				break;
-		}
+		let buttonHTML = this.innerHTML;
+		playSound(buttonHTML);
+		animateButton(buttonHTML);
 	});
+}
+document.addEventListener("keydown", function (event) {
+	buttonKey = event.key;
+	playSound(buttonKey);
+	animateButton(buttonKey);
+});
+function playSound(key) {
+	switch (key) {
+		case "w":
+			let tom1 = new Audio("./sounds/tom-1.mp3");
+			tom1.play();
+			break;
+		case "a":
+			let tom2 = new Audio("./sounds/tom-2.mp3");
+			tom2.play();
+			break;
+		case "s":
+			let tom3 = new Audio("./sounds/tom-3.mp3");
+			tom3.play();
+			break;
+		case "d":
+			let tom4 = new Audio("./sounds/tom-4.mp3");
+			tom4.play();
+			break;
+		case "j":
+			let crash = new Audio("./sounds/crash.mp3");
+			crash.play();
+			break;
+		case "k":
+			let kick = new Audio("./sounds/kick-bass.mp3");
+			kick.play();
+			break;
+		case "l":
+			let snare = new Audio("./sounds/snare.mp3");
+			snare.play();
+			break;
+		default:
+			console.log(key);
+			break;
+	}
+}
+
+function animateButton(currentKey) {
+	let activeButton = document.querySelector("." + currentKey);
+	activeButton.classList.toggle("pressed");
 }
